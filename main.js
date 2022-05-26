7842fdbd46c2d27dfd9dd125dc1aef926f4a4771
cvance248 <37356219+cvance248@users.noreply.github.com>
2022-05-25 22:18:51 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
+//API endpoint logic to check the price of whatever coin wants to be checked.
+
+//This is the button that will be used to retrieve data from the API once it is clicked.
 document.querySelector('button').addEventListener('click', getPrice)
 
 
 
 function getPrice () {
 
-    let symbol = document.querySelector('#cryptoSymbol').value.toUpperCase() + 'USDT'
+  //Makes variable for the imput value to be passed into the fetch URL
+    let cryptoInput = document.querySelector('#cryptoSymbol').value
+
+    //This is the container that result will be passed into
+    let cryptoAnswer = document.querySelector('#cryptoPrice')
+
+    //This will format the URL to retrieve the correct data
+    let symbol = cryptoInput.toUpperCase() + 'USDT'
 
 
     let url = `https://api.binance.com/api/v3/ticker/price?symbol=${symbol}`
@@ -14,12 +24,15 @@ fetch(url)
       .then(data => {
         console.log(data)
 
+        //Since the endpoint sends data back with 5 default decimals, I only want to show to decimal points for values that are greater than $1. This formats the returned dollar amount value give it a cleaner look
         Number(data.price) < 1 ? Number(data.price) : Number(data.price).toFixed(2)
 
-        document.querySelector('#cryptoPrice').textContent = `${data.symbol} is currently worth $ ${Number(data.price)} per coin`
+        //This is the template for the sentence that will be printed in the DOM to show that value of the selected coin in USD
+        cryptoAnswer.textContent = `${data.symbol} is currently worth $ ${Number(data.price)} per coin`
         
        
       })
+      //If an error occurs, it will be printed to the console
       .catch(err => {
           console.log(`error ${err}`)
       });
